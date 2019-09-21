724ef4b8e12ab33128c7f5fe8513d341905a07a6
David Ndungu <dnjuguna@gmail.com>
2019-09-21 12:55:06 -0700

    remove key rewrite logic

--- env.go
+++ env.go
@@ -2,13 +2,11 @@ package env
 
 import (
 	"os"
-	"strings"
 )
 
-// Get reads key from the environment. It will return (Value, true)
-// if the key exists or (nil, false) otherwise.
+// Get reads key from the environment, it returns (Value, true)
+// when the key exists or (nil, false) otherwise.
 func Get(key string) (Value, bool) {
-	key = strings.ToUpper(strings.ReplaceAll(key, "-", "_"))
 	var s, ok = os.LookupEnv(key)
 	return Value(s), ok
 }
