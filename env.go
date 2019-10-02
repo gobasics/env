dcf3949974ec78d6fae27934f06425deda4a8c2c
David Ndungu <dnjuguna@gmail.com>
2019-10-01 22:13:18 -0700

    rename Get to Lookup and create Get(string) Value

--- env.go
+++ env.go
@@ -4,9 +4,15 @@ import (
 	"os"
 )
 
-// Get reads key from the environment, it returns (Value, true)
+// Get reads key from the environment and returns Value
+func Get(key string) Value {
+	var s = os.Getenv(key)
+	return Value(s)
+}
+
+// Lookup reads key from the environment, it returns (Value, true)
 // when the key exists or (nil, false) otherwise.
-func Get(key string) (Value, bool) {
+func Lookup(key string) (Value, bool) {
 	var s, ok = os.LookupEnv(key)
 	return Value(s), ok
 }
